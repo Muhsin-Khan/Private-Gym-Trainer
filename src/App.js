8254d8e6d7a33de4a2ef6dddeb9e9a7f0bcb8cd6
Muhsin Khan <mdmuhsinuddin01@gmail.com>
2022-04-18 17:58:48 +0600

    after finishing few steps...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import app from './firebase.init';
+import {getAuth} from 'firebase/auth';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+
+const auth = getAuth(app);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+      <h1>kha kha kha morubhumi</h1>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXD6V5qKiNhQQg0WYjSq19XIzq6zOS-qA",
+  authDomain: "private-gym-trainer.firebaseapp.com",
+  projectId: "private-gym-trainer",
+  storageBucket: "private-gym-trainer.appspot.com",
+  messagingSenderId: "1063274301392",
+  appId: "1:1063274301392:web:7a84cd6929574f07959bd5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
